bf3b5f18983d9956fddc9d04d556a4fb958d057b
Anisina <makhmutovansk@gmail.com>
2023-11-29 01:35:02 +0400

    add new progect

--- src/pages/Projects/ui/ProjectsPage.tsx
+++ src/pages/Projects/ui/ProjectsPage.tsx
@@ -4,8 +4,10 @@ import laptop from "src/shared/assets/laptop_screen_transparent.png";
 import mobile from "src/shared/assets/mobile_screen_transparent.png";
 import backgroundImageProgect from "src/shared/assets/backgroundImageProgect.png";
 import backgroundImageProgect2 from "src/shared/assets/svarka.png";
+import backgroundImageProgect3 from "src/shared/assets/portfolio.png";
 import backgroundImageMobile from "src/shared/assets/mobile_btm.png";
 import backgroundImageMobile2 from "src/shared/assets/mobile_svarka.png";
+import backgroundImageMobile3 from "src/shared/assets/mobile_portfolio.png";
 
 const ProjectsPage = () => {
   const { t } = useTranslation("projects");
@@ -26,6 +28,14 @@ const ProjectsPage = () => {
       site: "https://svarkaraitkin.ru",
       title: "Svarka",
       text: "description_of_project2",
+    },
+    {
+      id: "portfolio_anisina",
+      image: backgroundImageProgect3,
+      imageMobile: backgroundImageMobile3,
+      site: "https://portfolio-anisina.vercel.app/",
+      title: "Portfolio Anisina",
+      text: "description_of_project3",
     },
   ];
 
